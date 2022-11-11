2242fbb361298040fed3e0af927ef91ac3926a84
Zeynab Hamad <zaynabhamad950@gmail.com>
2022-11-11 23:09:59 +0300

    compleated assignment

---
+++ index.js
@@ -0,0 +1,54 @@
+function add(){
+    
+}
+
+function subtract(){
+    
+}
+
+function multiply(){
+    
+}
+
+function divide(){
+    
+}
+
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+    
+}
+
+function multiply(a,b){
+    return a*b;
+    
+}
+
+function divide(a,b){
+    return a/b;
+    
+}
+
+function increment(n){
+  
+    return n+1;
+    
+}
+
+function decrement(n){
+    
+    return n-1;
+    
+}
+
+function makeInt(z){
+    return parseInt(z,10);
+}
+function preserveDecimal(z)
+{
+    return parseFloat(z);
+}
